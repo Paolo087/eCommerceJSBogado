5cbc5cea84b5ed95ab287b3050ecd112c05c7ed3
Paolo087 <pabogado87@gmail.com>
2022-12-07 21:05:14 -0300

    first commit

---
+++ src/Data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Vino",
+    price: 15.61,
+    desc: "Vino Ruca Malen Malbec tinto 750ml.",
+    img: "images/img-1.jpeg",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Cerveza",
+    price: 3.12,
+    desc: "Herken New-England IPA 330ml.",
+    img: "images/img-2.png",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "Ginebra",
+    price: 17.03,
+    desc: "Gin Bombay Sapphire 750ml.",
+    img: "images/img-3.jpeg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Whiskey",
+    price: 24.26,
+    desc: "Jack Daniel's Tennessee Whiskey.",
+    img: "images/img-4.jpeg",
+  },
+];
